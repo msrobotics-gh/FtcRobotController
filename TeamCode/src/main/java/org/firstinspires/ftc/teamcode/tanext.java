b815c8ecbd7706d9071111d8469ed33b78331317
roi bian <178917720+rbian1@users.noreply.github.com>
2025-10-23 17:35:16 -0400

    idk tbh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tanext.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tanext.java
@@ -7,10 +7,10 @@ import com.pedropathing.paths.Path;
 import com.pedropathing.paths.PathChain;
 import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import  com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 @Autonomous(name = "drive in line", group = "PP")
-public class ta extends OpMode {
+public class tanext extends OpMode {
 
     private Follower follower;
     private Timer pathTimer, actionTimer, opmodeTimer;
