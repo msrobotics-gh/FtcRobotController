1fde29134d9b1587991ec4ac607c460dd1e0271b
Leo <182694479+4321Blast@users.noreply.github.com>
2025-10-24 15:56:13 -0400

    Made the code better and fixed issues

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/FlywheelGate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/FlywheelGate.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.subsystems;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.groups.SequentialGroup;
+import dev.nextftc.core.commands.utility.LambdaCommand;
 import dev.nextftc.core.subsystems.Subsystem;
 import dev.nextftc.ftc.ActiveOpMode;
 import dev.nextftc.hardware.impl.ServoEx;
@@ -23,17 +25,34 @@ public class FlywheelGate implements Subsystem {
         gateServo = ActiveOpMode.hardwareMap().get(Servo.class, name);
         gateServoEx = new ServoEx(gateServo);
     }
-
-
+    
     public Command open() {
-        ActiveOpMode.telemetry().addLine("Opening");
-        ActiveOpMode.telemetry().update();
-        return new SetPosition(gateServoEx, // SERVO TO MOVE
-                0.0); // IMPLEMENTED SUBSYSTEM
+        return new SequentialGroup(
+            new SetPosition(
+                    gateServoEx, 0.0
+            ),
+
+        new LambdaCommand().setStart(
+                () -> {
+                    ActiveOpMode.telemetry().addLine("Opening");
+                    ActiveOpMode.telemetry().update();
+                }
+        )
+        );
     }
 
     public Command close() {
-        return new SetPosition(gateServoEx, // SERVO TO MOVE
-                0.3); // IMPLEMENTED SUBSYSTEM
+        return new SequentialGroup(
+                new SetPosition(
+                        gateServoEx, 0.3
+                ),
+
+                new LambdaCommand().setStart(
+                        () -> {
+                            ActiveOpMode.telemetry().addLine("Closing");
+                            ActiveOpMode.telemetry().update();
+                        }
+                )
+        );
     }
 }
