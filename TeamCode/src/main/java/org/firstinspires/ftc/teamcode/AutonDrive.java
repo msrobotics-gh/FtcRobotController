0fe2128569f9fc44615a5011c41a332540e81653
Atharv-Kumar-31 <atkumar@greenhillsschool.org>
2025-10-24 16:35:02 -0400

    Deleting Random Space

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonDrive.java
@@ -35,35 +35,11 @@
 
 
 package org.firstinspires.ftc.teamcode;
-
-
-
-
-
-
-
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*
  * This file contains a minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
  * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
