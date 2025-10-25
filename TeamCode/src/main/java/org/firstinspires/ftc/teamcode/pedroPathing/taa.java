553cf4c08ef51ecc2e10d06170c20e3cac09d112
roi bian <178917720+rbian1@users.noreply.github.com>
2025-10-25 16:01:01 -0400

    hi

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/taa.java
@@ -0,0 +1,92 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.ftc.FTCCoordinates;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.PedroCoordinates;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.util.Timer;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.Camera;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+
+import android.util.Size;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+
+@Autonomous(name="Turn to AprilTag", group="Autonomous")
+public class taa extends OpMode {
+    private Follower follower;
+    private Timer timer;
+    private int pathState;
+    private final Pose pose = new Pose(72,72,Math.toRadians(90));
+    private PathChain completePath;
+
+
+    // run repeatedly after clicking Start
+    @Override
+    public void loop() {
+        follower.update();
+        autonomousPathUpdate();
+
+        telemetry.addData("path state", pathState);
+        telemetry.addData("x", follower.getPose().getX());
+        telemetry.addData("y", follower.getPose().getY());
+        telemetry.addData("heading", follower.getPose().getHeading());
+        telemetry.update();
+
+        dashboardTelemetry.addData("path state", pathState);
+        dashboardTelemetry.addData("x", follower.getPose().getX());
+        dashboardTelemetry.addData("y", follower.getPose().getY());
+        dashboardTelemetry.addData("heading", follower.getPose().getHeading());
+        dashboardTelemetry.update();
+    }
+
+    // called once
+    @Override
+    public void init() {
+        pathTimer = new Timer();
+        opmodeTimer = new Timer();
+        opmodeTimer.resetTimer();
+
+
+        follower = Constants.createFollower(hardwareMap);
+        buildPaths();
+        follower.setStartingPose(pose1);
+
+    }
+
+    // called continuously while waiting for Start
+    @Override
+    public void init_loop() {}
+
+    // called once at start of opmode
+    @Override
+    public void start() {
+        opmodeTimer.resetTimer();
+        setPathState(0);
+    }
+
+    // dont use; everything auto disable
+    @Override
+    public void stop() {}
+
+}
