7d9d1a561d91a565969a028a9117d2c1d2ceda8c
Easton <234486409+eliska1111@users.noreply.github.com>
2025-10-30 16:24:59 -0400

    test auton

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuton.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.subsystems.FlywheelGate;
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.subsystems.Launcher;
+
+import dev.nextftc.core.commands.delays.Delay;
+import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.ftc.components.BulkReadComponent;
+
+@Autonomous(name = "simple auton launch")
+public class TestAuton extends NextFTCOpMode {
+    public TestAuton() {
+        addComponents(
+                BulkReadComponent.INSTANCE
+        );
+    }
+
+
+    @Override
+    public void onStartButtonPressed() {
+        Launcher.INSTANCE.spinflywheel.schedule();
+        Launcher.INSTANCE.spinflywheel2.schedule();
+        new Delay(0.5).schedule();
+        FlywheelGate.INSTANCE.open().schedule();
+        Intake.INSTANCE.intake.schedule();
+        Intake.INSTANCE.intakesecond.schedule();
+
+    }
+}
