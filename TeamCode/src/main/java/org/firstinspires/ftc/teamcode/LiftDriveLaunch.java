dff2ebb6d1bd3e16b196825702af037fdb241861
roi bian <178917720+rbian1@users.noreply.github.com>
2025-10-30 17:33:13 -0400

    nextftc implementation start

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftDriveLaunch.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftDriveLaunch.java
@@ -6,23 +6,27 @@ import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import org.firstinspires.ftc.teamcode.subsystems.FlywheelGate;
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.subsystems.Launcher;
 import org.firstinspires.ftc.teamcode.subsystems.Lift;
 //import org.firstinspires.ftc.teamcode.subsystems.Velauncher;
+//import org.firstinspires.ftc.teamcode.subsystems.Velauncher;
 
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.commands.CommandManager;
+import dev.nextftc.core.commands.utility.InstantCommand;
 import dev.nextftc.core.components.BindingsComponent;
 import dev.nextftc.core.components.SubsystemComponent;
+import dev.nextftc.extensions.pedro.PedroComponent;
 import dev.nextftc.ftc.Gamepads;
 import dev.nextftc.ftc.NextFTCOpMode;
 import dev.nextftc.ftc.components.BulkReadComponent;
 import dev.nextftc.hardware.driving.MecanumDriverControlled;
 import dev.nextftc.hardware.impl.MotorEx;
 
-@TeleOp(name = "lift drive launch")
+@TeleOp(name = "lift DRIVEEEEEE launch")
 @Config
 public class LiftDriveLaunch extends NextFTCOpMode {
     public LiftDriveLaunch() {
@@ -30,11 +34,13 @@ public class LiftDriveLaunch extends NextFTCOpMode {
                 new SubsystemComponent(Lift.INSTANCE),
                 BulkReadComponent.INSTANCE,
                 BindingsComponent.INSTANCE,
-                new SubsystemComponent(FlywheelGate.INSTANCE)
+                new SubsystemComponent(FlywheelGate.INSTANCE),
+                new PedroComponent(Constants::createFollower)
         );
     }
     FtcDashboard dashboard = FtcDashboard.getInstance();
-    Telemetry dashboardTelemetry = dashboard.getTelemetry();
+
+
 
     // change the names and directions to suit your robot
     private final MotorEx frontLeftMotor = new MotorEx("front_left");
@@ -47,6 +53,7 @@ public class LiftDriveLaunch extends NextFTCOpMode {
 
     public double bottommotorpower;
 
+    boolean isLeftBumperPressed = false;
     @Override
     public void onStartButtonPressed() {
         Command driverControlled = new MecanumDriverControlled(
@@ -67,13 +74,21 @@ public class LiftDriveLaunch extends NextFTCOpMode {
                 .whenBecomesTrue(Lift.INSTANCE.toLow);
 
         Gamepads.gamepad2().leftBumper()
-//                .whenTrue(Velauncher.INSTANCE.velaunch)
-//                .whenTrue(Velauncher.INSTANCE.velaunch2)
+                .whenTrue(Launcher.INSTANCE.spinflywheel)
+                .whenTrue(Launcher.INSTANCE.spinflywheel2)
+                .whenTrue(new InstantCommand(()-> {
+                    isLeftBumperPressed = true;
+                }))
                 .whenBecomesFalse(Launcher.INSTANCE.unspinflywheel)
-                .whenBecomesFalse(Launcher.INSTANCE.unspinflywheel2);
+                .whenBecomesFalse(Launcher.INSTANCE.unspinflywheel2)
+                .whenBecomesFalse(new InstantCommand(()-> {
+                    isLeftBumperPressed = false;
+                }));
+
 
 
         Gamepads.gamepad2().rightBumper()
+                .and(() -> isLeftBumperPressed)
                 .whenBecomesTrue(FlywheelGate.INSTANCE.open())
                 .whenBecomesTrue(Intake.INSTANCE.intake)
                 .whenBecomesTrue(Intake.INSTANCE.intakesecond)
@@ -90,7 +105,7 @@ public class LiftDriveLaunch extends NextFTCOpMode {
                 .whenBecomesTrue(Intake.INSTANCE.intakeoff2);
 
         for (String message : CommandManager.INSTANCE.snapshot()
-             ) {
+        ) {
             telemetry.addLine(message);
         }
         telemetry.update();
