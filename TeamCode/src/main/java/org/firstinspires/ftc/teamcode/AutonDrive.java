5eb9752ae2ba4a8b4c353de8008723cac48d7a8b
Atharv-Kumar-31 <atkumar@greenhillsschool.org>
2025-11-01 16:12:16 -0400

    Spaces Deleted

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonDrive.java
@@ -1,66 +1,9 @@
-/* Copyright (c) 2077 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-
-
-
-
-
-
-
 package org.firstinspires.ftc.teamcode;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
-/*
- * This file contains a minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When a selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
- */
-
-
-
-
-
-
-
-
 @Autonomous(name="Drive", group="Linear OpMode")
 public class AutonDrive extends LinearOpMode {
     // Declare OpMode members.
@@ -69,8 +12,8 @@ public class AutonDrive extends LinearOpMode {
     private DcMotor frontLeftDrive = null;
     private DcMotor frontRightDrive = null;
     double drive = -1;
-    double strafe = gamepad1.left_stick_x;
-    double turn  =  gamepad1.right_stick_x;
+    double strafe = 0;
+    double turn  =  0;
     private final ElapsedTime timer = new ElapsedTime();
     public int leftEncoderTicks = leftDrive.getCurrentPosition();
     public int rightEncoderTicks = rightDrive.getCurrentPosition();
@@ -80,8 +23,7 @@ public class AutonDrive extends LinearOpMode {
     public double rightPower;
     public double strafeLeftPower;
     public double strafeRightPower;
-
-    public void resetRotations() {
+     public void resetRotations() {
         leftEncoderTicks = 0;
         rightEncoderTicks = 0;
         frontLeftEncoderTicks = 0;
@@ -89,21 +31,14 @@ public class AutonDrive extends LinearOpMode {
     }
 
     public void drive(double speed) {
-    }
 
+    }
     final double COUNTS_PER_MOTOR_REV = 28; // Example for HD Hex Motor, adjust for your motor
 
     double leftRotations = (double) leftEncoderTicks / COUNTS_PER_MOTOR_REV;
     double rightRotations = (double) rightEncoderTicks / COUNTS_PER_MOTOR_REV;
     double frontLeftRotations = (double) frontLeftEncoderTicks / COUNTS_PER_MOTOR_REV;
     double frontRightRotations = (double) frontRightEncoderTicks / COUNTS_PER_MOTOR_REV;
-
-
-
-
-
-
-
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry.addData("Status", "Initialized");
@@ -206,7 +141,7 @@ public class AutonDrive extends LinearOpMode {
 
 
             // Show the elapsed game timer and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Status", "Run Time: " );
             telemetry.addData("Motors", "left (%.2f)", leftPower);
             telemetry.update();
         }
