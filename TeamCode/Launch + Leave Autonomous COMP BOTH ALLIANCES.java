d3f860d0e734d56870873f68433a6717705333b1
Soham Kathula <102703232+sohamyk@users.noreply.github.com>
2025-11-14 21:25:07 -0500

    Add autonomous mode for driving and launching

---
+++ TeamCode/Launch + Leave Autonomous COMP BOTH ALLIANCES.java
@@ -0,0 +1,139 @@
+package org.firstinspires.ftc.teamcode.Mechanisms;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="Autonomous - Drive + Launch", group="Linear OpMode")
+public class RandomAuton extends LinearOpMode {
+
+    // Drive Motors
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+    private DcMotor frontLeftDrive = null;
+    private DcMotor frontRightDrive = null;
+
+    // Intake
+    private DcMotor leftIntake = null;
+    private DcMotor rightIntake = null;
+
+    // Launcher
+    private DcMotor upDrive = null;
+    private DcMotor downDrive = null;
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+
+
+        leftDrive      = hardwareMap.get(DcMotor.class, "left_bottom");
+        rightDrive     = hardwareMap.get(DcMotor.class, "right_bottom");
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "left_top");
+        frontRightDrive= hardwareMap.get(DcMotor.class, "right_top");
+
+        upDrive        = hardwareMap.get(DcMotor.class, "up");
+        downDrive      = hardwareMap.get(DcMotor.class, "down");
+
+        leftIntake     = hardwareMap.get(DcMotor.class, "rightt");
+        rightIntake    = hardwareMap.get(DcMotor.class, "leftt");
+
+
+
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+        frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        leftIntake.setDirection(DcMotor.Direction.REVERSE);
+        rightIntake.setDirection(DcMotor.Direction.FORWARD);
+
+        upDrive.setDirection(DcMotor.Direction.REVERSE);
+        downDrive.setDirection(DcMotor.Direction.FORWARD);
+
+
+        leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontLeftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+
+        waitForStart();
+        runtime.reset();
+
+        if (opModeIsActive()) {
+
+
+            setDrivePower(0.8, 0.8, 0.8, 0.8);
+            sleep(810);
+            stopDrive();
+
+
+            sleep(300);
+
+
+            double launchPower = 1;
+
+            upDrive.setPower(-0.85);
+            downDrive.setPower(launchPower);
+
+
+            sleep(1000);
+
+
+            leftIntake.setPower(1.0);
+            rightIntake.setPower(1.0);
+            sleep(670);
+            leftIntake.setPower(0);
+            rightIntake.setPower(0);
+            sleep(670);
+            leftIntake.setPower(1.0);
+            rightIntake.setPower(1.0);
+            sleep(670);
+            leftIntake.setPower(0);
+            rightIntake.setPower(0);
+            sleep(670);
+            leftIntake.setPower(1.0);
+            rightIntake.setPower(1.0);
+            sleep(670);
+            leftIntake.setPower(0);
+            rightIntake.setPower(0);
+            sleep(670);
+            rightIntake.setPower(1.0);
+
+
+            sleep(1000);
+;
+
+            leftIntake.setPower(0);
+            rightIntake.setPower(0);
+            upDrive.setPower(0);
+            downDrive.setPower(0);
+            stopDrive();
+
+            setDrivePower(0.75, 0.75, 0.75, 0.75);
+            sleep(500);
+            stopDrive();
+            telemetry.addData("Status", "Autonomous Complete");
+            telemetry.update();
+        }
+    }
+
+
+    private void setDrivePower(double lb, double rb, double lf, double rf) {
+        leftDrive.setPower(lb);
+        rightDrive.setPower(rb);
+        frontLeftDrive.setPower(lf);
+        frontRightDrive.setPower(rf);
+    }
+
+
+    private void stopDrive() {
+        setDrivePower(0, 0, 0, 0);
+    }
+}
