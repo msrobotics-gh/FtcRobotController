dc494a7105f4ac0ddf140f7d71286a197db69099
Soham Kathula <102703232+sohamyk@users.noreply.github.com>
2025-11-21 16:43:36 -0500

    Rename class from ChelseaWeHaveYourClipBoard to Auton

--- TeamCode/Launch + Leave Autonomous COMP BOTH ALLIANCES.java
+++ TeamCode/Launch + Leave Autonomous COMP BOTH ALLIANCES.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name="Autonomous - Drive + Launch", group="Linear OpMode")
-public class ChelseaWeHaveYourClipBoard extends LinearOpMode {
+public class Auton extends LinearOpMode {
 
     // Drive Motors
     private DcMotor leftDrive = null;
